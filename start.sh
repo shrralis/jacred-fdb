62472940064d2b909cfeb0a354c2178d10b77d8a
shrralis <root@shrralis.com>
2024-04-30 15:21:39 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 readonly custom_conf_filepath='./custom_conf/init.conf'
-if [ ! -f "${custom_conf_filepath}" ]; then
+if [ -f "${custom_conf_filepath}" ]; then
     cp -f "${custom_conf_filepath}" './init.conf'
 fi
 
